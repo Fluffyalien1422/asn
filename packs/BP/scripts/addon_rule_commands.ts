0fef25cacf39bdd4b1d1c64ff52c702131c696da
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2025-01-04 16:03:42 -0800

    feat: better addon rules experience - more color in help screen and other messages - the new value is sent in chat after it the command is executed - added experimental and deprecation warning messages - showRequestItemDialog is now deprecated

---
+++ packs/BP/scripts/addon_rule_commands.ts
@@ -0,0 +1,228 @@
+import { Player, world } from "@minecraft/server";
+import {
+  forceLoadNetworksRule,
+  showRequestItemDialogRule,
+  useEnergyRule,
+  wirelessInterfaceRangeRule,
+} from "./addon_rules";
+import { DynamicPropertyAccessor } from "./utils/dynamic_property";
+
+interface BaseAddonRuleCommand {
+  deprecated?: boolean;
+  experimental?: boolean;
+}
+
+interface BoolAddonRuleCommand extends BaseAddonRuleCommand {
+  type: "bool";
+  property: DynamicPropertyAccessor<boolean, boolean>;
+}
+
+interface NumberAddonRuleCommand extends BaseAddonRuleCommand {
+  type: "float" | "int";
+  property: DynamicPropertyAccessor<number, number>;
+}
+
+type AddonRuleCommand = BoolAddonRuleCommand | NumberAddonRuleCommand;
+
+const ADDON_RULE_COMMANDS: Record<string, AddonRuleCommand> = {
+  forceLoadNetworks: {
+    type: "bool",
+    property: forceLoadNetworksRule,
+  },
+  showRequestItemDialog: {
+    type: "bool",
+    property: showRequestItemDialogRule,
+    deprecated: true,
+  },
+  wirelessInterfaceRange: {
+    type: "int",
+    property: wirelessInterfaceRangeRule,
+  },
+  useEnergy: {
+    type: "bool",
+    property: useEnergyRule,
+    experimental: true,
+  },
+};
+
+function processBoolAddonRuleCommand(
+  player: Player,
+  rawValue: string,
+  ruleCommand: BoolAddonRuleCommand,
+): boolean {
+  if (rawValue === "true") {
+    ruleCommand.property.set(world, true);
+    return true;
+  }
+
+  if (rawValue === "false") {
+    ruleCommand.property.set(world, false);
+    return true;
+  }
+
+  player.sendMessage({
+    rawtext: [
+      {
+        text: "§c",
+      },
+      {
+        translate:
+          "fluffyalien_asn.message.scriptEvent.addonRule.expectedBoolean",
+      },
+    ],
+  });
+
+  return false;
+}
+
+function processNumberAddonRuleCommand(
+  player: Player,
+  rawValue: string,
+  ruleCommand: NumberAddonRuleCommand,
+): boolean {
+  const numVal = Number(rawValue);
+
+  if (isNaN(numVal)) {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§c",
+        },
+        {
+          translate:
+            "fluffyalien_asn.message.scriptEvent.addonRule.expectedNumber",
+        },
+      ],
+    });
+
+    return false;
+  }
+
+  if (ruleCommand.type === "int" && !Number.isInteger(numVal)) {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§c",
+        },
+        {
+          translate:
+            "fluffyalien_asn.message.scriptEvent.addonRule.expectedInt",
+        },
+      ],
+    });
+
+    return false;
+  }
+
+  ruleCommand.property.set(world, numVal);
+
+  return true;
+}
+
+function sendCurrentRuleValue(
+  player: Player,
+  rule: string,
+  ruleCommand: AddonRuleCommand,
+): void {
+  player.sendMessage(
+    `§s${rule}§r = §p${ruleCommand.property.get(world).toString()}`,
+  );
+}
+
+export function processAddonRuleCommand(player: Player, message: string): void {
+  const args = message.split(" ");
+
+  const rule = args[0];
+  const value = args[1];
+
+  if (rule === "help") {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§a-- fluffyalien_asn:addonrule help -- §r\n",
+        },
+        ...Object.entries(ADDON_RULE_COMMANDS).flatMap(([key, options]) => [
+          {
+            text: `§s${key}§r: §u${options.type} §5(default: §p${options.property.defaultValue.toString()}§5)§r${options.deprecated ? " §c(deprecated)§r" : ""}${options.experimental ? " §c(experimental)§r" : ""} - `,
+          },
+          {
+            translate: `fluffyalien_asn.message.scriptEvent.addonRule.help.${key}`,
+          },
+          {
+            text: "\n§a-- --§r\n",
+          },
+        ]),
+      ],
+    });
+
+    return;
+  }
+
+  if (!(rule in ADDON_RULE_COMMANDS)) {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§c",
+        },
+        {
+          translate:
+            "fluffyalien_asn.message.scriptEvent.addonRule.invalidRule",
+        },
+      ],
+    });
+    return;
+  }
+
+  const ruleCommand = ADDON_RULE_COMMANDS[rule];
+
+  if (!value) {
+    sendCurrentRuleValue(player, rule, ruleCommand);
+    return;
+  }
+
+  let success = false;
+
+  switch (ruleCommand.type) {
+    case "bool":
+      success = processBoolAddonRuleCommand(player, value, ruleCommand);
+      break;
+    case "float":
+    case "int":
+      success = processNumberAddonRuleCommand(player, value, ruleCommand);
+      break;
+  }
+
+  if (!success) {
+    return;
+  }
+
+  sendCurrentRuleValue(player, rule, ruleCommand);
+
+  if (ruleCommand.deprecated) {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§c",
+        },
+        {
+          translate:
+            "fluffyalien_asn.message.scriptEvent.addonRule.deprecatedWarning",
+        },
+      ],
+    });
+  }
+
+  if (ruleCommand.experimental) {
+    player.sendMessage({
+      rawtext: [
+        {
+          text: "§c",
+        },
+        {
+          translate:
+            "fluffyalien_asn.message.scriptEvent.addonRule.experimentalWarning",
+        },
+      ],
+    });
+  }
+}
--- packs/BP/scripts/script_events.ts
+++ packs/BP/scripts/script_events.ts
@@ -1,57 +1,20 @@
 import { getPlayerMainhandSlot } from "./utils/item";
 import { STORAGE_DATA_DYNAMIC_PROPERTY_ID } from "./storage_drive";
-import { system, Player, world } from "@minecraft/server";
+import { system, Player } from "@minecraft/server";
 import { logWarn } from "./log";
-import {
-  forceLoadNetworksRule,
-  showRequestItemDialogRule,
-  useEnergyRule,
-  wirelessInterfaceRangeRule,
-} from "./addon_rules";
-import { DynamicPropertyAccessor } from "./utils/dynamic_property";
-
-type AddonRuleCommand =
-  | {
-      type: "bool";
-      property: DynamicPropertyAccessor<boolean, boolean>;
-    }
-  | {
-      type: "number";
-      int?: boolean;
-      property: DynamicPropertyAccessor<number, number>;
-    };
-
-const ADDON_RULE_COMMANDS: Record<string, AddonRuleCommand> = {
-  forceLoadNetworks: {
-    type: "bool",
-    property: forceLoadNetworksRule,
-  },
-  showRequestItemDialog: {
-    type: "bool",
-    property: showRequestItemDialogRule,
-  },
-  wirelessInterfaceRange: {
-    type: "number",
-    int: true,
-    property: wirelessInterfaceRangeRule,
-  },
-  useEnergy: {
-    type: "bool",
-    property: useEnergyRule,
-  },
-};
+import { processAddonRuleCommand } from "./addon_rule_commands";
 
 system.afterEvents.scriptEventReceive.subscribe(
   (e) => {
     if (!(e.sourceEntity instanceof Player)) return;
     const player = e.sourceEntity;
 
-    if (e.id === "fluffyalien_asn:debug_log_disk_data") {
+    if (e.id === "fluffyalien_asn:debug.log_disk_data") {
       const item = getPlayerMainhandSlot(player).getItem();
 
       if (item?.typeId !== "fluffyalien_asn:used_storage_disk") {
         logWarn(
-          "could not run script event fluffyalien_asn:debug_log_disk_data: not holding fluffyalien_asn:used_storage_disk",
+          "could not run script event fluffyalien_asn:debug.log_disk_data: not holding fluffyalien_asn:used_storage_disk",
         );
         return;
       }
@@ -59,118 +22,9 @@ system.afterEvents.scriptEventReceive.subscribe(
       const s = item
         .getDynamicProperty(STORAGE_DATA_DYNAMIC_PROPERTY_ID)
         ?.toString();
-      if (s) logWarn(`fluffyalien_asn:debug_log_disk_data result: ${s}`);
+      if (s) logWarn(`fluffyalien_asn:debug.log_disk_data result: ${s}`);
     } else if (e.id === "fluffyalien_asn:addonrule") {
-      const args = e.message.split(" ");
-
-      const rule = args[0];
-      const value = args[1];
-
-      if (rule === "help") {
-        player.sendMessage({
-          rawtext: [
-            {
-              text: "§a-- fluffyalien_asn:addonrule help -- §r\n",
-            },
-            ...Object.keys(ADDON_RULE_COMMANDS).flatMap((key) => [
-              {
-                translate: `fluffyalien_asn.message.scriptEvent.addonRule.help.${key}`,
-              },
-              {
-                text: "\n§a-- --§r\n",
-              },
-            ]),
-          ],
-        });
-
-        return;
-      }
-
-      if (!(rule in ADDON_RULE_COMMANDS)) {
-        player.sendMessage({
-          rawtext: [
-            {
-              text: "§c",
-            },
-            {
-              translate:
-                "fluffyalien_asn.message.scriptEvent.addonRule.invalidRule",
-            },
-          ],
-        });
-        return;
-      }
-
-      const ruleCommand = ADDON_RULE_COMMANDS[rule];
-
-      if (!value) {
-        player.sendMessage(
-          `${rule} = ${ruleCommand.property.get(world).toString()}`,
-        );
-        return;
-      }
-
-      if (ruleCommand.type === "bool") {
-        if (value === "true") {
-          ruleCommand.property.set(world, true);
-          return;
-        }
-
-        if (value === "false") {
-          ruleCommand.property.set(world, false);
-          return;
-        }
-
-        player.sendMessage({
-          rawtext: [
-            {
-              text: "§c",
-            },
-            {
-              translate:
-                "fluffyalien_asn.message.scriptEvent.addonRule.expectedBoolean",
-            },
-          ],
-        });
-
-        return;
-      }
-
-      const numVal = Number(value);
-
-      if (isNaN(numVal)) {
-        player.sendMessage({
-          rawtext: [
-            {
-              text: "§c",
-            },
-            {
-              translate:
-                "fluffyalien_asn.message.scriptEvent.addonRule.expectedNumber",
-            },
-          ],
-        });
-
-        return;
-      }
-
-      if (ruleCommand.int && !Number.isInteger(numVal)) {
-        player.sendMessage({
-          rawtext: [
-            {
-              text: "§c",
-            },
-            {
-              translate:
-                "fluffyalien_asn.message.scriptEvent.addonRule.expectedInt",
-            },
-          ],
-        });
-
-        return;
-      }
-
-      ruleCommand.property.set(world, numVal);
+      processAddonRuleCommand(player, e.message);
     } else {
       player.sendMessage({
         rawtext: [
