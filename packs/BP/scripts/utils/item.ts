e0fb187e5242406837ed4a3bc6020e5a18c3f189
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-03-17 17:54:48 -0700

    feat: use localization key property for item translations

--- packs/BP/scripts/utils/item.ts
+++ packs/BP/scripts/utils/item.ts
@@ -1,20 +1,10 @@
 import {
-  BlockPermutation,
   ContainerSlot,
   Enchantment,
   EquipmentSlot,
+  ItemStack,
   Player,
 } from "@minecraft/server";
-import { ITEM_TRANSLATION_OVERRIDES } from "../item_translation_overrides";
-
-export function isBlock(itemId: string): boolean {
-  try {
-    BlockPermutation.resolve(itemId);
-    return true;
-  } catch {
-    return false;
-  }
-}
 
 export function getPlayerMainhandSlot(player: Player): ContainerSlot {
   return player
@@ -23,20 +13,7 @@ export function getPlayerMainhandSlot(player: Player): ContainerSlot {
 }
 
 export function getItemTranslationKey(itemId: string): string {
-  if (itemId in ITEM_TRANSLATION_OVERRIDES) {
-    return ITEM_TRANSLATION_OVERRIDES[itemId];
-  }
-
-  const isMinecraftNamespace = itemId.startsWith("minecraft:");
-  const translationKeyItemId = isMinecraftNamespace
-    ? itemId.slice("minecraft:".length)
-    : itemId;
-
-  return isBlock(itemId)
-    ? `tile.${translationKeyItemId}.name`
-    : isMinecraftNamespace
-      ? `item.${translationKeyItemId}.name`
-      : `item.${translationKeyItemId}`;
+  return new ItemStack(itemId).localizationKey;
 }
 
 export function getEnchantmentTypeId(enchantment: Enchantment): string {
