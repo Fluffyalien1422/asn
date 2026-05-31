449806e481ac440e5f4adf24baa112b1877c9ddb
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-05-31 13:58:00 -0700

    docs: update TODOs

--- packs/BP/scripts/index.ts
+++ packs/BP/scripts/index.ts
@@ -9,10 +9,11 @@ import "./v3storagetest";
 //TODO: update network when a disk is added or removed from a drive
 //TOOD: ui for drive
 //TODO: removing half (right click) of an item doesn't count as an interaction and allows you to duplicate an item
-//TODO: update sorting (group similar items together)
-//TODO: update code, stop using legacy results
+//TODO: update sorting (group stacks of same item type together)
+//TODO: update legacy code, stop using legacy 'Result'
 //TODO: remove standalone build and portable storage network
 //TODO: different disk sizes
 //TODO: remove legacy drives and disks
 //TODO: update storage interface ui. ideas:
 // grouped ui: show all item types (by id) and when clicked, show all stacks of that item. also add an option to view all stacks of all items, which takes you to the current ui.
+//TODO: ensure fluid storage works with new systems
