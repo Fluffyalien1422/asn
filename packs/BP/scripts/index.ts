2e850bb8b1c02555f6b1e35dcc01d41d9352ff09
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-06-05 20:51:14 -0700

    docs: update TODOs

--- packs/BP/scripts/index.ts
+++ packs/BP/scripts/index.ts
@@ -4,6 +4,9 @@ import "./script_events";
 import "./tutorial_book";
 import "./wireless_interface";
 
-//TODO: different disk sizes
+//TODO: enable useEnergy by default, but keep the rule if players want to disable it.
+//TODO: remove fluidStorage, showRequestItemDialog, and forceLoadNetworks rules. fluidStorage and forceLoadNetworks will be always enabled and cannot be disabled (the rule should be removed). showRequestItemDialog is legacy and should be removed entirely.
+//TODO: different disk sizes (64 and 32?)
 //TODO: remove legacy drives and disks
-//TODO: ensure fluid storage works with new systems
+//TODO: update legacy bus code and improve export bus and level emitter ui (use an inventory based ui?)
+//TODO: test everything: buses, fluid storage, wireless interfaces, relays, etc
