836d21602cc2474c2bd66da5efaa5548b15e5286
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-06-23 17:11:55 -0700

    refactor: small changes

--- packs/BP/scripts/index.ts
+++ packs/BP/scripts/index.ts
@@ -7,6 +7,6 @@ import "./wireless_interface";
 //TODO: test everything: buses, fluid storage, wireless interfaces, relays, etc
 //TODO: rebalance energy consumption
 
+//TODO: autocrafter tutorial book entry
 //TODO: disk upgrader - upgrades a 32 disk to 64. preserves the items stored in the 32 disk. requires energy (unless 'useEnergy' is disabled).
-//TODO: autocrafter - connects to the storage network and automatically crafts items. interact with an item to set the recipe. can be disabled with redstone.
 //TODO: update disk textures
