0f45ecda19796163b6a6b3190a375fd6e8b0a6dd
Fluffyalien1422 <94992989+Fluffyalien1422@users.noreply.github.com>
2026-07-08 11:07:58 -0700

    refactor: update disk_placeholder texture

--- packs/BP/scripts/index.ts
+++ packs/BP/scripts/index.ts
@@ -3,6 +3,3 @@ import "./custom_components";
 import "./energistics";
 import "./tutorial_book";
 import "./wireless_interface";
-
-//TODO: rebalance energy consumption
-//TODO: update storage disk placeholder texture
